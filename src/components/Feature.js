b38f10d9355240a7b666647e8f02e3b38e5e70a1
Yudi <singh.yudi10@gmail.com>
2023-06-21 23:43:56 +0530

    Add icons to features

--- src/components/Feature.js
+++ src/components/Feature.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
-const Feature = ({ title, description }) => {
+const Feature = ({ title, description, icon }) => {
   return (
     <div style={featureStyle}>
+      <img src={icon} alt="Feature Icon" style={iconStyle} />
       <h3>{title}</h3>
       <p>{description}</p>
     </div>
@@ -11,6 +12,11 @@ const Feature = ({ title, description }) => {
 
 const featureStyle = {
   maxWidth: '400px',
+};
+
+const iconStyle = {
+  width: '50px',
+  height: '50px',
 };
 
 export default Feature;
--- src/components/TestimonialsSection.js
+++ src/components/TestimonialsSection.js
@@ -2,32 +2,108 @@ import React from 'react';
 import Testimonial from './Testimonial';
 
 const testimonials = [
-  {
-    rating: 4.8,
-    testimonial: 'The college admission process was seamless. I received excellent guidance and support throughout. Thank you!',
-    name: 'Aarav Sharma',
-  },
-  {
-    rating: 4.5,
-    testimonial: "I'm grateful for the assistance I received. The counselors provided valuable insights and helped me secure admission.",
-    name: 'Anaya Patel',
-  },
-  {
-    rating: 4.2,
-    testimonial: 'The college application process was simplified and stress-free. The team helped me find the best fit for my goals.',
-    name: 'Arjun Gupta',
-  },
-  {
-    rating: 4.9,
-    testimonial: "I couldn't have asked for better support. The consultants were knowledgeable and guided me towards success.",
-    name: 'Aanya Reddy',
-  },
-  {
-    rating: 4.7,
-    testimonial: 'The college admission services exceeded my expectations. They helped me stand out and secure admission to my dream college.',
-    name: 'Advait Kumar',
-  },
-];
+    {
+      "rating": 4.8,
+      "testimonial": "The college admission process was seamless. I received excellent guidance and support throughout. Thank you!",
+      "name": "Aarav Sharma"
+    },
+    {
+      "rating": 4.5,
+      "testimonial": "I'm grateful for the assistance I received. The counselors provided valuable insights and helped me secure admission.",
+      "name": "Anaya Patel"
+    },
+    {
+      "rating": 4.2,
+      "testimonial": "The college application process was simplified and stress-free. The team helped me find the best fit for my goals.",
+      "name": "Arjun Gupta"
+    },
+    {
+      "rating": 4.9,
+      "testimonial": "I couldn't have asked for better support. The consultants were knowledgeable and guided me towards success.",
+      "name": "Aanya Reddy"
+    },
+    {
+      "rating": 4.7,
+      "testimonial": "The college admission services exceeded my expectations. They helped me stand out and secure admission to my dream college.",
+      "name": "Advait Kumar"
+    },
+    {
+      "rating": 4.5,
+      "testimonial": "I'm thrilled with my college admission. The team provided personalized guidance that made all the difference.",
+      "name": "Aishwarya Singh"
+    },
+    {
+      "rating": 4.3,
+      "testimonial": "The counselors were supportive and knowledgeable. They helped me navigate the complex college admission process.",
+      "name": "Aryan Verma"
+    },
+    {
+      "rating": 4.9,
+      "testimonial": "Thanks to the college admission consultants, I secured admission to my dream college. I couldn't be happier!",
+      "name": "Diya Desai"
+    },
+    {
+      "rating": 4.6,
+      "testimonial": "The college admission assistance was invaluable. They helped me choose the right college and guided me at every step.",
+      "name": "Ishaan Joshi"
+    },
+    {
+      "rating": 4.4,
+      "testimonial": "I'm grateful for the expert guidance I received. They helped me craft a compelling application and get accepted.",
+      "name": "Kavya Sharma"
+    },
+    {
+      "rating": 4.7,
+      "testimonial": "The college admission team was efficient and supportive. They helped me present my best self and secure admission.",
+      "name": "Manvi Kapoor"
+    },
+    {
+      "rating": 4.2,
+      "testimonial": "I appreciate the personalized attention I received. The consultants helped me find the best college fit for my aspirations.",
+      "name": "Neha Patel"
+    },
+    {
+      "rating": 4.8,
+      "testimonial": "The college admission process was made easier with their expert guidance. I'm thrilled with the outcome.",
+      "name": "Rohan Gupta"
+    },
+    {
+      "rating": 4.5,
+      "testimonial": "I'm thankful for the support I received. They helped me navigate the competitive college admission process successfully.",
+      "name": "Sara Khan"
+    },
+    {
+      "rating": 4.3,
+      "testimonial": "The college admission consultants were knowledgeable and helpful. They guided me towards making the right choices.",
+      "name": "Shaurya Singh"
+    },
+    {
+      "rating": 4.9,
+      "testimonial": "I highly recommend their college admission services. They provided personalized guidance and helped me get admitted to a top college.",
+      "name": "Shreya Sharma"
+    },
+    {
+      "rating": 4.4,
+      "testimonial": "I'm thankful for the college admission assistance I received. They helped me present my profile effectively.",
+      "name": "Vedant Patel"
+    },
+    {
+      "rating": 4.7,
+      "testimonial": "The college admission team was efficient and supportive. They helped me present my best self and secure admission.",
+      "name": "Zara Ahmed"
+    },
+    {
+      "rating": 4.6,
+      "testimonial": "Thanks to the college admission consultants, I got accepted into my dream college. Their guidance was invaluable.",
+      "name": "Ravi Kapoor"
+    },
+    {
+      "rating": 4.8,
+      "testimonial": "The college admission process was made stress-free with their expertise. I'm grateful for their support.",
+      "name": "Priya Sharma"
+    }
+  ]
+  
 
 const TestimonialsSection = () => {
   const testimonialContainerStyle = {
